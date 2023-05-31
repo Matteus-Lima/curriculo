f749153be4b9cec97e88b09793273bd93ec0c204
matteus lima <matteuslimq@gmail.com>
2023-05-31 20:34:29 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const animatedInfos = document.querySelectorAll('.animated-info');
+  
+    animatedInfos.forEach(info => {
+      info.classList.add('animated-info');
+    });
+  });
+  
